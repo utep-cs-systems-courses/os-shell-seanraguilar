cc116e715493fa91f2bb6fc31650c18200551a9f
student <sraguilar4@miners.utep.edu>
2021-02-14 22:00:06 -0700

    Final Touches

--- shell/Lab1.py
+++ shell/Lab1.py
@@ -4,42 +4,42 @@ import os, sys, re
 
 def main():
     
-    while True: #trying to replicate what a shell does 
+    while True: # This is trying to replicate what a shell actually does 
         
         if 'PS1' in os.environ:
             os.write(1, (os.environ['PS1']).encode())
         else:
             os.write(1, ("$ ").encode())
             
-        args = os.read(0, 1024)
+        args = os.read(0, 1024) # The number of bytes that can be accepted
         
         if len(args) == 0:
-            break #exits while loop
+            break # This exits while loop
         
         args = args.decode().split("\n")
         
         if not args:
-            continue # goes back to start of while loop
+            continue # This goes back to start of while loop
         
         for arg in args:
             inputHandler(arg.split())
             
 def inputHandler(args):
   
-    if len(args) == 0: # do nothing, return to main
+    if len(args) == 0: # Nothing really happens here, return to main
         return
         
     if "exit" in args: 
         sys.exit(0)
 
-    # change directory
+    # This is the change directory
     elif "cd" == args[0]:
         try:
-            if len(args)==1: # if just cd is specified, reprompt the user
+            if len(args)==1: # If cd is specified, reprompt the user
                 return   
             else:
                 os.chdir(args[1])
-        except: # nonexistent
+        except: # Does not existent
             os.write(1, ("cd %s: No such file or directory\n" % args[1]).encode())
     else:
         rc = os.fork()
@@ -59,7 +59,7 @@ def executeCommand(args):
         except FileNotFoundError:
             pass
         
-    os.write(2, ("%s: command not found\n" % args[0]).encode()) # command not found, print an error message
+    os.write(2, ("%s: command not found\n" % args[0]).encode()) # There is command not found, print an error message
     sys.exit(0)
 
 if '__main__' == __name__:
