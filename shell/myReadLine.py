61e76fe42876ca7303973a456f24a25e296d7d59
Sean Ryan Aguilar <55062065+seanraguilar@users.noreply.github.com>
2021-02-21 20:45:47 -0700

    Update myReadLine.py

--- shell/myReadLine.py
+++ shell/myReadLine.py
@@ -2,7 +2,7 @@
 
 from os import read
 
-ibuf = ""      # Input buffer, 
+ibuf = ""      # Input buffer
 sbuf = ""      # String buffer
 sbufLength = 0 # String buffer length
 currChar = 0   # Index of current char in sbuf
@@ -18,7 +18,7 @@ def getChar():
         sbuf = ibuf.decode()
         sbufLength = len(sbuf) # The length of the string
         currChar = 0
-        if sbufLength == 0:    # If we reached the end of the input then it would return nothing
+        if sbufLength == 0: # If we reached the end of the input then it would return nothing
             return ''
     
     char = sbuf[currChar]
@@ -29,17 +29,16 @@ def readLine():
     char = getChar()
     line = ""
     
-    while char != '\n':     # While char is not equal to new line, keep getting chars for line
+    while char != '\n': # While char is not equal to new line, keep getting chars for line
         line += char
         char = getChar()
-        if char == '':      # If char is empty, then we reached EOF; retun
+        if char == '': # If char is empty, then we reached EOF; retun
             return line
-    line+= '\n'             # If a new line was found, then return the line with a new line char
-    return line #was return not print
+    line+= '\n' # If a new line was found, then return the line with a new line char
+    return line # Was return the line, not print
 
 def main():
     readLine()
-    
+        
 if '__main__' == __name__:
     main() 
-    
